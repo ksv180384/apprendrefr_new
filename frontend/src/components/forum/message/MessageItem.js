3f054c066c779046b39d50df3162fef0896a1bd9
Sergey Kotelevsky <sk@webberry.ru>
2020-06-30 17:23:00 +0300

    правки верстки форума

--- frontend/src/components/forum/message/MessageItem.js
+++ frontend/src/components/forum/message/MessageItem.js
@@ -11,14 +11,14 @@ class MessagesList extends Component{
 
             <div className="MessageItem">
                 <div className="MessageItem-user-info">
-                    <div className="MessageItem-user-login">
-                        { message.user_login }
-                    </div>
                     <div className="MessageItem-user-avatar">
                         <div className="MessageItem-user-avatar-block"
                              style={ {backgroundImage: 'url('+ message.user_avatar +')'} }>
 
                         </div>
+                    </div>
+                    <div className="MessageItem-user-login">
+                        { message.user_login }
                     </div>
                     <div className="MessageItem-user-group">
                         { message.user_rang_title }
